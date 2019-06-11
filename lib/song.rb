9f9cb8c5622983aed0b04f74a10a69f8e201b551
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-11 22:05:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,14 +9,11 @@ def initialize(name)
 end
 
 def self.new_by_filename(filename)
-  artist_name, song_name = filename.chomp(" .mp3").split(" - ")
-  song = Song.new(song_name)
-  song.artist = Artist.find_or_create_by_name(artist_name)
-  song.save
-  # song = Song.new(filename.split(" - ")[1])
-  # song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
-  # song.artist.add_song(song)
-  # song
+  
+  song = Song.new(filename.split(" - ")[1])
+  song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+  song.artist.add_song(song)
+  song
 end
 
 
