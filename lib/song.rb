2c4ca1c8dbf3ca749e08319539c68d41063c0120
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-11 22:40:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ def self.new_by_filename(filename)
 end
 
 def artist_name=(name)
-  self.artist = Artist.find_or_create_by_name(name)
+  artist = Artist.find_or_create_by_name(name)
   artist.add_song(self)
   end
 end
