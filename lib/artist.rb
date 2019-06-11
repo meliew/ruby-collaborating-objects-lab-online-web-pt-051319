a3a6600289f51dadd914184552b75047627cb662
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-11 21:41:53 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,46 @@
+class Artist
+
+attr_accessor :name, :songs
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @songs = []
+end
+
+def add_song(song)
+@songs << song
+song.artist = self
+end
+
+def songs
+  @songs
+end
+
+def save
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+
+def self.find_or_create_by_name(name)
+  #iterate over the artists
+
+  self.all.find{|artist| artist.name == name || artist = Artist.new(name) }
+
+end
+
+def print_songs
+@songs.each do |song|
+  puts song.name
+  end
+
+end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+
+attr_accessor :name, :artist
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all = []
+end
+
+def self.new_by_filename(filename)
+song = Song.new(filename.split(" - ")[1])
+song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+song
+end
+
+end
