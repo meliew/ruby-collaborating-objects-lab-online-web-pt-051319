2d6f2ebc43b573296d4cf618e6415411d273cada
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-11 21:48:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ end
 def self.new_by_filename(filename)
 song =
 self.new
-songname = filename.split(" - ")[1])
+songname = filename.split(" - ")[1]
 song.artistname = filename.split(" - ")[0]
 song.artist = Artist.find_or_create_by_name(name)
 song
